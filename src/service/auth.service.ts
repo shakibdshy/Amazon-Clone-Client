0b27a1a5882b81ac0f4160a135c52783a60a2db6
Md Habibur Rahman <shakibdshy@gmail.com>
2022-09-04 20:34:49 +0600

    Update server location

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { DisplayUser } from "../type/user";
 
 const register = async (newUser: DisplayUser): Promise<DisplayUser | null> => {
-    const { data } = await axios.post(`http://localhost:8080/auth/register`, newUser);
+    const { data } = await axios.post(`http://localhost:8800/auth/register`, newUser);
 
     console.log(data);
     
